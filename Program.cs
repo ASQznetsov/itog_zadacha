15f9957dd784f850c6574a1d196d0c21734b350a
ASKuznetsov <etokuznetsov@gmail.com>
2022-11-20 11:32:29 +0300

    Ввел массив из строк

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using static System.Console;
+Clear();
+string[] str1;
+WriteLine("Введите массив строк через пробел");
+string line = ReadLine();
+str1 = line.Split(' ');
+
+string[] str2 = new string[str1.Length];
